37b13be540b5cea58205b7d37b57246fe9773723
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-08-16 20:41:48 -0300

    comparar texto

---
+++ algoritmos/strings/CompararStrings.java
@@ -0,0 +1,30 @@
+package algoritmos.strings;
+
+import java.util.Scanner;
+
+public class CompararStrings {
+
+	public static void main(String[] args) {
+		
+		String texto1, texto2;
+		Scanner s = new Scanner(System.in);
+		texto1=s.nextLine();
+		texto2="10";
+		// código não recomendado		
+		if (texto1==texto2) {
+			System.out.println("Deu Certo");
+		}else {
+			System.out.println("Deu errado");
+		}
+		
+		if (texto1.equals(texto2)) {
+			System.out.println("Deu Certo, são iguais");
+		}else {
+			System.out.println("Deu errado");
+		}
+		
+		//não use ==, >, >=, <, <= para comparar texto
+		// equals , compareTo
+	}
+
+}
