ea86a79c7762db8564ad24559697c4f53830dc4a
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-08-16 20:18:38 -0300

    Leitura e erro de buffer

---
+++ algoritmos/strings/LeituraTeclado.java
@@ -0,0 +1,19 @@
+package algoritmos.strings;
+
+import java.util.Scanner;
+
+public class LeituraTeclado {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String texto1 = s.next(); // lê toda a linha até o ENTER
+		String texto2 = s.next(); // lê apenas uma palavra
+		String texto3 = s.next(); // lê apenas uma palavra		
+		
+		System.out.println(texto1);
+		System.out.println(texto2);
+		System.out.println(texto3);
+
+	}
+
+}
--- algoritmos/strings/ProblemaBufferTecladoString.java
+++ algoritmos/strings/ProblemaBufferTecladoString.java
@@ -8,13 +8,18 @@ public class ProblemaBufferTecladoString {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Digite o número");
 		int x = s.nextInt();
-		//s.nextLine();
 		clearBuffer(s);
+		 // s.nextLine(); 
+		
 		System.out.println("Digite a frase");
-		String texto6 = s.nextLine(); // todo o texto da linha
+		String texto1 = s.nextLine(); // todo o texto da linha
 		        
 		System.out.println("Digite a palavra");
-		String texto7 = s.next(); // a primeira palavra
+		String texto2 = s.next(); // a primeira palavra
+		
+		System.out.println(x);
+		System.out.println(texto1);
+		System.out.println(texto2);
 
 	}
 	
