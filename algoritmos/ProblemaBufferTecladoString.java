ee06699dc7465baf1ddebad13fddaa0ecfe90342
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-08-16 16:35:24 -0300

    Inicializando Repositório

---
+++ algoritmos/ProblemaBufferTecladoString.java
@@ -0,0 +1,27 @@
+package algoritmos;
+
+import java.util.Scanner;
+
+public class ProblemaBufferTecladoString {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o número");
+		int x = s.nextInt();
+		//s.nextLine();
+		clearBuffer(s);
+		System.out.println("Digite a frase");
+		String texto6 = s.nextLine(); // todo o texto da linha
+		        
+		System.out.println("Digite a palavra");
+		String texto7 = s.next(); // a primeira palavra
+
+	}
+	
+	public static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+    }
+
+}
