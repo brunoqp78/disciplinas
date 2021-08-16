6e18c8d2f4da1c16d7165849cd3f99b0b1e16cef
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-08-16 20:54:04 -0300

    Tamanho de String

---
+++ algoritmos/strings/TamanhoStrings.java
@@ -0,0 +1,15 @@
+package algoritmos.strings;
+
+public class TamanhoStrings {
+
+	public static void main(String[] args) {
+		int vetor[] = new int[10];
+		
+		String nome = "Bruno Queiroz";
+		
+		System.out.println(vetor.length);
+		System.out.println(nome.length());
+
+	}
+
+}
